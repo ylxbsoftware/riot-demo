7a72030c7d8dbb8270824f3bdf22c430830a656b
Wan Bao <ylxbsoftware@163.com>
2016-06-26 11:43:37 +0800

    modify some code

--- app/main/main.js
+++ app/main/main.js
@@ -2,8 +2,11 @@ require('zepto/zepto.min.js');
 require('./main.tag')
 require('weui');
 $.ajax({
-    url: 'app/data/user.json',
-    success: function(data) {
-        riot.mount('main', {title:'会员列表', data: data })
-    }
+  // url: 'app/data/user.json',
+  url: 'http://192.168.1.105:8080',
+  dataType: 'json',
+  success: function(data) {
+    riot.mount('main', { title: '会员列表', data: data });
+  }
 });
+
